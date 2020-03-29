69bbdee35025f15337d809872d5c2c27bc171928
Caizhi Xu <xucaizhi@gmail.com>
2020-03-29 13:11:56 -0700

    updates

---
+++ 1. Two Sum _ array.py
@@ -0,0 +1,9 @@
+## time, space - O(N)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pre = {}
+        for i, num in enumerate(nums):
+            if target - num in pre:
+                return [pre[target - num], i]
+            else:
+                pre[num] = i
