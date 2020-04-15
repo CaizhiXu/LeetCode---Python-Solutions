ce09099cc355266b72feca1b78570138afeeba45
Caizhi Xu <xucaizhi@gmail.com>
2020-04-15 12:07:12 -0700

    updates

--- 282. Expression Add Operators _ string.py
+++ 282. Expression Add Operators _ string.py
@@ -1,47 +1,37 @@
 class Solution:
     def addOperators(self, num: 'str', target: 'int') -> 'List[str]':
-
-        N = len(num)
-        answers = []
-        def recurse(index, prev_operand, current_operand, value, string):
-            print(index, prev_operand, current_operand, value, string)
-
-            # Done processing all the digits in num
-            if index == N:
-
-                # If the final value == target expected AND
-                # no operand is left unprocessed
-                if value == target and current_operand == 0:
-                    answers.append("".join(string[1:]))
+        def backtrack(index, pre_op, curr_op, value, string):
+            if index == len(num):
+                if value == target and curr_op == 0:
+                    res.append(''.join(string[1:]))
                 return
 
-            # Extending the current operand by one digit
-            current_operand = current_operand*10 + int(num[index])
-            str_op = str(current_operand)
+            curr_op = curr_op * 10 + int(num[index])
+            if curr_op > 0:
+                backtrack(index + 1, pre_op, curr_op, value, string)
 
-            # To avoid cases where we have 1 + 05 or 1 * 05 since 05 won't be a
-            # valid operand. Hence this check
-            if current_operand > 0:
+            string.append('+')
+            string.append(str(curr_op))
+            backtrack(index + 1, curr_op, 0, value + curr_op, string)
+            string.pop()
+            string.pop()
 
-                # NO OP recursion
-                recurse(index + 1, prev_operand, current_operand, value, string)
-
-            # ADDITION
-            string.append('+'); string.append(str_op)
-            recurse(index + 1, current_operand, 0, value + current_operand, string)
-            string.pop();string.pop()
-
-            # Can subtract or multiply only if there are some previous operands
             if string:
+                string.append('-')
+                string.append(str(curr_op))
+                backtrack(index + 1, -curr_op, 0, value - curr_op, string)
+                string.pop()
+                string.pop()
 
-                # SUBTRACTION
-                string.append('-'); string.append(str_op)
-                recurse(index + 1, -current_operand, 0, value - current_operand, string)
-                string.pop();string.pop()
+                string.append('*')
+                string.append(str(curr_op))
+                backtrack(index + 1, pre_op * curr_op, 0, value - pre_op + \
+                          pre_op * curr_op, string)
+                string.pop()
+                string.pop()
 
-                # MULTIPLICATION
-                string.append('*'); string.append(str_op)
-                recurse(index + 1, current_operand * prev_operand, 0, value - prev_operand + (current_operand * prev_operand), string)
-                string.pop();string.pop()
-        recurse(0, 0, 0, 0, [])
-        return answers
+        res = []
+        backtrack(0, 0, 0, 0, [])
+        return res
+
+
--- 438. Find All Anagrams in a String _ string.py
+++ 438. Find All Anagrams in a String _ string.py
@@ -4,27 +4,23 @@ from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p_cnts = Counter(p)
-        s_cnts = {}
-        left, right = 0, 0
+        window = Counter(p)
+        cnt = len(window)
+        size = len(p)
+        left = 0
         res = []
-        match = 0
 
-        while right < len(s):
-            s_cnts[s[right]] = s_cnts.get(s[right], 0) + 1
-            if s_cnts[s[right]] == p_cnts[s[right]]:
-                match += 1
-            while s_cnts[s[right]] > p_cnts[s[right]]:
-                s_cnts[s[left]] -= 1
-                if s_cnts[s[left]] < p_cnts[s[left]]:
-                    match -= 1
+        for right, c in enumerate(s):
+            if c in window:
+                window[c] -= 1
+                if window[c] == 0:
+                    cnt -= 1
+            if right - left >= size:
+                if s[left] in window:
+                    window[s[left]] += 1
+                    if window[s[left]] == 1:
+                        cnt += 1
                 left += 1
-
-            if match == len(p_cnts):
+            if cnt == 0:
                 res.append(left)
-                s_cnts[s[left]] -= 1
-                match -= 1
-                left += 1
-
-            right += 1
         return res
