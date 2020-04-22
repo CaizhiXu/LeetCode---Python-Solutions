23e8469e71f4b96bb76018c9cc7af2a1d54a1ddf
Caizhi Xu <xucaizhi@gmail.com>
2020-04-22 12:04:20 -0700

    updates

--- 282. Expression Add Operators _ string.py
+++ 282. Expression Add Operators _ string.py
@@ -35,3 +35,24 @@ class Solution:
         return res
 
 
+## time - O(N*4**N), space - O(N)
+class Solution:
+    def addOperators(self, num: 'str', target: 'int') -> 'List[str]':
+        res = []
+        self.target = target
+        for i in range(1, len(num) + 1):
+            if i == 1 or num[0] != '0':
+                self.dfs(num[:i], num[i:], int(num[:i]), int(num[:i]), res)
+        return res
+
+    def dfs(self, path, arr, last, curr, res):
+        if not arr:
+            if curr == self.target:
+                res.append(path)
+            return
+        for i in range(1, len(arr) + 1):
+            if i == 1 or arr[0] != '0':
+                tmp = arr[:i]
+                self.dfs(path + '+' + tmp, arr[i:], int(tmp), curr + int(tmp), res)
+                self.dfs(path + '-' + tmp, arr[i:], -int(tmp), curr - int(tmp), res)
+                self.dfs(path + '*' + tmp, arr[i:], last * int(tmp), curr - last + last * int(tmp), res)
