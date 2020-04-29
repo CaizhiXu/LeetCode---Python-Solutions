cf3ebbcf9c73add97f66ca738edbec3b880c62d8
Caizhi Xu <xucaizhi@gmail.com>
2020-04-29 00:57:38 -0700

    updates

--- 279_perfect squares.py
+++ 279_perfect squares.py
@@ -15,7 +15,7 @@ class Solution:
         self.memo[n] = res
         return res
 
-## sol2 - dp
+## sol2 - dp, time - O(n**1.5), space - O(n)
 class Solution2(object):
     def numSquares(self, n):
         dp = [i for i in range(n+1)]
@@ -23,6 +23,17 @@ class Solution2(object):
             for j in range(curr*curr,n+1):
                 dp[j] = min(dp[j],dp[j-curr*curr]+1)
         return dp[-1]
+
+## sol3 - dp
+class Solution3:
+    def numSquares(self, n: int) -> int:
+        dp = [i for i in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, int(i ** 0.5) + 1):
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+
+        return dp[n]
 
 
 ## sol3 - BFS, fastest solution
@@ -40,3 +51,26 @@ class Solution(object):
                         break
                     sub_q.add(node - num)
             q, res = sub_q, res+1
+
+
+## another bfs, time - O(n), space - O(n**0.5)
+from collections import deque
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = [i * i for i in range(1, int(n ** 0.5) + 1)]
+        dq = deque([(n, 0)])
+        visited = set([n])
+
+        while dq:
+            num, dep = dq.pop()
+            if num == 0:
+                return dep
+            for s in squares:
+                if s < num:
+                    if num - s not in visited:
+                        dq.appendleft((num - s, dep + 1))
+                        visited.add(num - s)
+                elif s == num:
+                    return dep + 1
+                else:
+                    break
