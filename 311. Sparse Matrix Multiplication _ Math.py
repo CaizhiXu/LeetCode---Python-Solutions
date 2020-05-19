5c859da56fd88db6d7b4bf33621e02404dc6856c
Caizhi Xu <xucaizhi@gmail.com>
2020-05-18 21:35:05 -0700

    updates

--- 311. Sparse Matrix Multiplication _ Math.py
+++ 311. Sparse Matrix Multiplication _ Math.py
@@ -13,3 +13,28 @@ class Solution:
                             C[i][k] += A[i][j] * B[j][k]
 
         return C
+
+
+## time - less than m*n*nB, space - m*nB
+class Solution:
+    def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        if not A or not A[0] or not B or not B[0]:
+            return None
+        m, n = len(A), len(A[0])
+        k = len(B[0])
+        if len(B) != n:
+            raise Exception("Dimension mismatch!")
+
+        res = [[0] * k for i in range(m)]
+        nonZero = {}
+        for i, row in enumerate(B):
+            nonZero[i] = {}
+            for j in range(len(row)):
+                if row[j] != 0:
+                    nonZero[i][j] = row[j]
+        for i, row in enumerate(A):
+            for k, eleA in enumerate(row):
+                if eleA != 0:
+                    for j, eleB in nonZero[k].items():
+                        res[i][j] += eleA * eleB
+        return res
