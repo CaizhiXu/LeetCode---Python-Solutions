8b874026119a1f4300533f8d761b5da049530e32
Caizhi Xu <xucaizhi@gmail.com>
2020-05-24 14:39:12 -0700

    updates

--- 114. Flatten Binary Tree to Linked List _ Tree.py
+++ 114. Flatten Binary Tree to Linked List _ Tree.py
@@ -22,3 +22,30 @@ class Solution:
             node.left = None
             if stack:
                 node.right = stack[-1]
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+## time - O(n), space - O(1)
+
+class Solution2:
+    def flatten(self, root: TreeNode) -> None:
+        self.helper(root)
+
+    def helper(self, node):
+        if not node:
+            return None
+        if not node.left and not node.right:
+            return node
+
+        lefttail = self.helper(node.left)
+        righttail = self.helper(node.right)
+        if lefttail:
+            lefttail.right = node.right
+            node.right = node.left
+            node.left = None
+        return righttail if righttail else lefttail
