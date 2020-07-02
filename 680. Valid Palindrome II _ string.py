1394b548d0e6b6b172bdb9e5e285f928a626d976
Caizhi Xu <xucaizhi@gmail.com>
2020-07-01 17:23:35 -0700

    updates

--- 680. Valid Palindrome II _ string.py
+++ 680. Valid Palindrome II _ string.py
@@ -23,3 +23,26 @@ class Solution:
             else:
                 return False
         return True
+
+
+## time - O(n), space - O(1)
+class Solution2:
+    def validPalindrome(self, s: str) -> bool:
+        if not s:
+            return True
+        n = len(s)
+        for i in range(n // 2):
+            if s[i] == s[n - 1 - i]:
+                continue
+            else:
+                return self.isPalindrome(s, i, n - 1 - i - 1) or \
+                       self.isPalindrome(s, i + 1, n - 1 - i)
+        return True
+
+    def isPalindrome(self, s, i, j):
+        while i <= j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
